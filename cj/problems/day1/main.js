779a672ef871b899d4e459048134c110754bfcee
yeongjong310 <tohero21@naver.com>
2022-01-06 19:29:50 +0900

    refactor: html 클래스 명, 변수 명 변경
    이유:
    더 명확한 쓰임을 나타내기 위함 및 구조 통일화

--- cj/problems/day1/main.js
+++ cj/problems/day1/main.js
@@ -1,54 +1,65 @@
-const items = document.querySelector('.items');
-const input = document.querySelector('.footer__input');
-const addBtn = document.querySelector('.footer__button');
+const $todos = document.querySelector(".todos");
+const $input = document.querySelector(".footer__input");
+const $addBtn = document.querySelector(".footer__button");
+
+function createTodoItem(text) {
+  // 변수명을 더 명확하게 하기 위해 itemRow를 $todoItem으로 변수명 변경
+  const $todoItem = document.createElement("li");
+  $todoItem.setAttribute("class", "todo__item");
+
+  // 변수명을 더 명확하게 하기 위해 item을 $todoItemStruct으로 변수명 변경
+  const $todoItemStruct = document.createElement("div");
+  $todoItemStruct.setAttribute("class", "todo__item--wrapper");
+
+  // 변수명을 더 명확하게 하기 위해 title을 $todoItemTitle으로 변수명 변경
+  const $todoItemTitle = document.createElement("span");
+  $todoItemTitle.setAttribute("class", "todo__item--title");
+  $todoItemTitle.innerText = text;
+
+  // 변수명을 더 명확하게 하기 위해 deleteBtn을 $todoItemDeleteBtn으로 변수명 변경
+  const $todoItemDeleteBtn = document.createElement("button");
+  $todoItemDeleteBtn.setAttribute("class", "todo__item--delete-btn");
+  $todoItemDeleteBtn.innerHTML = '<i class="fas fa-trash-alt"></i>';
+  $todoItemDeleteBtn.addEventListener("click", (e) => {
+    console.log;
+    const selected = e.currentTarget.closest(".todo__item");
+    selected.remove();
+  });
+
+  $todoItemStruct.appendChild($todoItemDeleteBtn);
+
+  // DOM 키워드 통일화를 위해 itemDivider를 $itemDivider로 변경
+  const $itemDivider = document.createElement("div");
+  $itemDivider.setAttribute("class", "todo__item--divider");
+
+  $todoItemStruct.appendChild($todoItemTitle);
+  $todoItemStruct.scrollIntoView({ block: "center" });
+
+  $todoItem.appendChild($todoItemStruct);
+  $todoItem.appendChild($itemDivider);
+
+  return $todoItem;
+}
 
 function onAdd() {
-  const text = input.value;
-  if (text === '') {
-    input.focus();
+  const text = $input.value;
+  if (text === "") {
+    $input.focus();
     return;
   }
 
-  const itemRow = document.createElement('li');
-  itemRow.setAttribute('class', 'item__row');
+  $todos.appendChild(createTodoItem(text));
 
-  const item = document.createElement('div');
-  item.setAttribute('class', 'item');
-
-  const name = document.createElement('span');
-  name.setAttribute('class', 'item__name');
-  name.innerText = text;
-
-  const deleteBtn = document.createElement('button');
-  deleteBtn.setAttribute('class', 'item__delete');
-  deleteBtn.innerHTML = '<i class="fas fa-trash-alt"></i>';
-
-  const itemDivider = document.createElement('div');
-  itemDivider.setAttribute('class', 'item__divider');
-
-  item.appendChild(name);
-  item.appendChild(deleteBtn);
-
-  itemRow.appendChild(item);
-  itemRow.appendChild(itemDivider);
-
-  items.appendChild(item);
-  item.scrollIntoView({ block: 'center' });
-  input.value = '';
-  input.focus();
-
-  deleteBtn.addEventListener('click', (e) => {
-    const selected = e.currentTarget.parentNode;
-    selected.remove();
-  });
+  $input.value = "";
+  $input.focus();
 }
 
-addBtn.addEventListener('click', () => {
+$addBtn.addEventListener("click", () => {
   onAdd();
 });
 
-input.addEventListener('keypress', (event) => {
-  if (event.key === 'Enter') {
+$input.addEventListener("keypress", (event) => {
+  if (event.key === "Enter") {
     onAdd();
   }
 });
