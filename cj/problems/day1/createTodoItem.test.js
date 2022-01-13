2b44af40dc987e43f160f803fe02b614fd8bde7c
yeongjong310 <tohero21@naver.com>
2022-01-13 14:40:50 +0900

    refactor: 데이터 생성 부 모듈화 및 테스트 파일명 변경

--- cj/problems/day1/createTodoItem.test.js
+++ cj/problems/day1/createTodoItem.test.js
@@ -2,7 +2,7 @@
  * @jest-environment jsdom
  */
 
-import { createTodoItem } from "./main.js";
+import { createTodoItem } from "./createTodoItem.js";
 
 test("createTodoItem('dom test 해보기')", () => {
   expect(createTodoItem("dom test 해보기")).toMatchSnapshot();
--- cj/problems/day1/main.js
+++ cj/problems/day1/main.js
@@ -1,62 +1,30 @@
-export function createTodoItem(text) {
-  // 변수명을 더 명확하게 하기 위해 itemRow를 $todoItem으로 변수명 변경
-  const $todoItem = document.createElement("li");
-  $todoItem.setAttribute("class", "todo__item");
-
-  // 변수명을 더 명확하게 하기 위해 item을 $todoItemStruct으로 변수명 변경
-  const $todoItemStruct = document.createElement("div");
-  $todoItemStruct.setAttribute("class", "todo__item--wrapper");
-
-  // 변수명을 더 명확하게 하기 위해 title을 $todoItemTitle으로 변수명 변경
-  const $todoItemTitle = document.createElement("span");
-  $todoItemTitle.setAttribute("class", "todo__item--title");
-  $todoItemTitle.textContent = text;
-
-  // 변수명을 더 명확하게 하기 위해 deleteBtn을 $todoItemDeleteBtn으로 변수명 변경
-  const $todoItemDeleteBtn = document.createElement("button");
-  $todoItemDeleteBtn.setAttribute("class", "todo__item--delete-btn");
-  $todoItemDeleteBtn.innerHTML = '<i class="fas fa-trash-alt"></i>';
-  $todoItemDeleteBtn.addEventListener("click", (e) => {
-    const selected = e.currentTarget.closest(".todo__item");
-    selected.remove();
-  });
-
-  $todoItemStruct.appendChild($todoItemDeleteBtn);
-
-  // DOM 키워드 통일화를 위해 itemDivider를 $itemDivider로 변경
-  const $itemDivider = document.createElement("div");
-  $itemDivider.setAttribute("class", "todo__item--divider");
-
-  $todoItemStruct.appendChild($todoItemTitle);
-  $todoItem.appendChild($todoItemStruct);
-  $todoItem.appendChild($itemDivider);
-
-  return $todoItem;
-}
+import { createTodoItem } from "./createTodoItem";
 
 const $todos = document.querySelector(".todos");
 const $input = document.querySelector(".footer__input");
 const $addBtn = document.querySelector(".footer__button");
 
-// function onAdd() {
-//   const text = $input.value;
-//   if (text === "") {
-//     $input.focus();
-//     return;
-//   }
+function onAdd() {
+  const text = $input.value;
+  if (text === "") {
+    $input.focus();
+    return;
+  }
 
-//   $todos.appendChild(createTodoItem(text));
+  $todos.appendChild(createTodoItem(text));
 
-//   $input.value = "";
-//   $input.focus();
-// }
+  $input.value = "";
+  $input.focus();
+}
 
-// $addBtn.addEventListener("click", () => {
-//   onAdd();
-// });
+// TODO jest 통과하도록 수정하기.
+// TODO jest 설정방법 블로그에 문서화 하기.
+$addBtn.addEventListener("click", () => {
+  onAdd();
+});
 
-// $input.addEventListener("keypress", (event) => {
-//   if (event.key === "Enter") {
-//     onAdd();
-//   }
-// });
+$input.addEventListener("keypress", (event) => {
+  if (event.key === "Enter") {
+    onAdd();
+  }
+});
